91aaa1ba91aa448f7e92cae0d22211e6ea19ade1
Kamil Balda <k.balda@foreto.com>
2021-08-27 08:08:16 +0200

    restaurantsDAO updated to see data from DB

--- backend/api/restaurants.controller.js
+++ backend/api/restaurants.controller.js
@@ -1,33 +1,36 @@
-import RestaurantsDAO from '../dao/restaurantsDAO'
+import RestaurantsDAO from "../dao/restaurantsDAO.js";
 
-export default class RestaurantsContoller{
+export default class RestaurantsContoller {
   static async apiGetRestaurants(req, res, next) {
-    const restaurantsPerPage = req.query.restaurantsPerPage ? parseInt(req.query.restaurantsPerPage, 10) : 20
-    const page = req.query.page ? parseInt(req.query.page, 10) : 0
+    const restaurantsPerPage = req.query.restaurantsPerPage
+      ? parseInt(req.query.restaurantsPerPage, 10)
+      : 20;
+    const page = req.query.page ? parseInt(req.query.page, 10) : 0;
 
-    let filters = {}
-    if(req.query.cuisine){
-      filters.cuisine = req.query.cuisine
+    let filters = {};
+    if (req.query.cuisine) {
+      filters.cuisine = req.query.cuisine;
     } else if (req.query.zipcode) {
-      filters.zipcode = req.query.zipcode
-    } else if (req.query.name){
-      filters.name = req.query.name
+      filters.zipcode = req.query.zipcode;
+    } else if (req.query.name) {
+      filters.name = req.query.name;
     }
 
-    const { restaurantsList, totalNumRestaurants } = await RestaurantsDAO.getRestaurants({
-      filters,
-      page,
-      restaurantsPerPage
-    })
+    const { restaurantsList, totalNumRestaurants } =
+      await RestaurantsDAO.getRestaurants({
+        filters,
+        page,
+        restaurantsPerPage,
+      });
 
     let response = {
       restaurants: restaurantsList,
       page: page,
       filters: filters,
       entries_per_page: restaurantsPerPage,
-      total_results: totalNumRestaurants
-    }
+      total_results: totalNumRestaurants,
+    };
 
-    res.json(response)
-  } 
+    res.json(response);
+  }
 }
