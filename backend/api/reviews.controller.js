d6926a8e552bb837de3bed7a1773dbf86b442b1e
balda-kamil <baldakamil@gmail.com>
2021-08-27 10:42:08 +0200

    front end start

--- backend/api/reviews.controller.js
+++ backend/api/reviews.controller.js
@@ -1,5 +1,4 @@
 import ReviewsDAO from "../dao/reviewsDAO.js"
-
 export default class ReviewsController {
   static async apiPostReview(req, res, next) {
     try {
---
+++ front-end/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Switch, Route, Link} from 'react-router-dom'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hejo!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
