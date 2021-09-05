ec4c365510c574cca41ce63d1aa30b1556de5f0b
balda-kamil <baldakamil@gmail.com>
2021-09-05 09:07:29 +0200

    commit before batery will gone ;) user reg on front in progress

--- front-end/src/components/register.js
+++ front-end/src/components/register.js
@@ -3,37 +3,49 @@ import React from "react"
 const Register = props => {
   console.log(props)
 
-  const [user, setUser] = React.useState();
+  const [formData, updateFormData] = React.useState();
 
-  const register = () => {
+  const handleSubmit = e => {
+    console.log(e)
+    e.preventDefault()
     console.log('register function')
+
+    
   }
 
+  const handleChange = e => {
+    console.log(e)
+    updateFormData({
+      ...formData,
+
+      // Trimming any whitespace
+      [e.target.name]: e.target.value.trim()
+    });
+  }
+
+  console.log(formData)
 
   return (
     <div className="submit-form">
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>
             First Name:
-            <input type="text" name="fname" />
+            <input type="text" name="fname" onChange={handleChange}/>
           </label>
           <label>
             Last Name:
-            <input type="text" name="lname" />
+            <input type="text" name="lname" onChange={handleChange}/>
           </label>
           <label>
             Email
-            <input type="email" name="email" />
+            <input type="email" name="email" onChange={handleChange}/>
           </label>
           <label>
             Password:
-            <input type="password" name="fname" />
+            <input type="password" name="password" onChange={handleChange}/>
           </label>
           <input type="submit" value="Submit" />
-        <button onClick={register} className="btn btn-success">
-          Sign Up!
-        </button>
         </form>
       </div>
     </div>
